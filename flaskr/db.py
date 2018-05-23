504c0e9e40f0300f94d412c0d22a0348b95ab4a8
gcisneros <gcisneros@jakks.net>
2018-05-23 16:47:19 -0700

    added init database function

--- flaskr/db.py
+++ flaskr/db.py
@@ -22,3 +22,23 @@ def close_db():
 
     if db is not None:
         db.close()
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read.decode('utf8'))
+
+# defines the command line 'init-db'
+# to call the init_db function
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    init_db()
+    click.echo('Initialized the database')
+
+def init_app(app):
+    # to call close_db when cleaning up after returning the response
+    app.teardown_appcontext(close_db)
+    # adds a new command that can be calles with the flask command
+    app.cli.add_command(init_db_command)
