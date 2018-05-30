2de22013b25389bbc487e92b5a22573f3c13597a
gcisneros <gcisneros@jakks.net>
2018-05-30 14:34:34 -0700

    database setup and schema

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,23 +1,23 @@
 import sqlite3
 import click
 
-# g is a special object that's unique for each request.
+# g is a special object that's unique for each request;
 # It's used to store data that might be accessed by multiple functions during the request.
 from flask import current_app, g
 from flask.cli import with_appcontext
 
 def get_db():
     if 'db' not in g:
-        # have to make a new db connection
         g.db = sqlite3.connect(
-            current_app.config['DATABASE']
+            current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
+        # tells the connection to return rows that behave like dicts
         g.db.row_factory = sqlite3.Row
 
-    return g.db # just returns existing db if available
+    return g.db
 
-def close_db():
+def close_db(e=None):
     db = g.pop('db', None)
 
     if db is not None:
@@ -26,8 +26,10 @@ def close_db():
 def init_db():
     db = get_db()
 
+    # points to the Flask application handling the request
+    # b/c we used an application factory
     with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read.decode('utf8'))
+        db.executescript(f.read().decode('utf8'))
 
 # defines the command line 'init-db'
 # to call the init_db function
@@ -40,5 +42,5 @@ def init_db_command():
 def init_app(app):
     # to call close_db when cleaning up after returning the response
     app.teardown_appcontext(close_db)
-    # adds a new command that can be calles with the flask command
+    # adds a new command that can be called with the flask command
     app.cli.add_command(init_db_command)
